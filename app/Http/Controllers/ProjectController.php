a892d7611b1b2955b7d2efcaf3a2eb41d06fd7b0
saepudinasep <saepudinasep2001@gmail.com>
2024-08-26 12:39:35 +0700

    [UPDATE] Update create image_path

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\UpdateProjectRequest;
 use App\Http\Resources\ProjectResource;
 use App\Http\Resources\TaskResource;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class ProjectController extends Controller
 {
@@ -53,8 +54,17 @@ class ProjectController extends Controller
     public function store(StoreProjectRequest $request)
     {
         $data = $request->validated();
+
+        /** @var \Illuminate\Http\UploadedFile|null $image */
+        $image = $data['image'] ?? null;
+
         $data['created_by'] = Auth::id();
         $data['updated_by'] = Auth::id();
+
+        if ($image) {
+            $data['image_path'] = $image->store('project/' . Str::random(), 'public');
+        }
+
         Project::create($data);
 
         return to_route('project.index')->with('success', 'Project was created');
