6a9c1233955d32f966305f833eb66431f0e6ef2b
saepudinasep <saepudinasep2001@gmail.com>
2024-08-29 09:34:08 +0700

    [UPDATE] Merubah tampilan dashboard

--- resources/js/Pages/Task/Show.jsx
+++ resources/js/Pages/Task/Show.jsx
@@ -14,9 +14,17 @@ export default function Show({ auth, task, tasks, queryParams }) {
     <AuthenticatedLayout
       user={auth.user}
       header={
-        <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-          {`Task "${task.name}"`}
-        </h2>
+        <div className="flex items-center justify-between">
+          <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {`Task "${task.name}"`}
+          </h2>
+          <Link
+            href={route("task.edit", task.id)}
+            className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600"
+          >
+            Edit
+          </Link>
+        </div>
       }
     >
       <Head title={`Task "${task.name}"`} />
